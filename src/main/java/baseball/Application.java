c3d100ba198bf7c4752e5af334ec41fd8dd1f696
ch200203 <ch200205@naver.com>
2023-06-27 01:48:40 +0900

    feat(baseball) : 기능 완성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,12 +1,46 @@
 package baseball;
 
+import baseball.domain.Judgement;
+import baseball.domain.NumberGenerator;
+import baseball.domain.Referee;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
 /**
- *         1. 기능을 가지고 있는 크래스를 인스턴스화(=객체) 한다.
- *         2. 필요한 기능을 각 인스턴스가 수행하게 한다. (의인화)
- *         3. 각 결과를 합친다.
+ * 1. 기능을 가지고 있는 크래스를 인스턴스화(=객체) 한다. 2. 필요한 기능을 각 인스턴스가 수행하게 한다. (의인화) 3. 각 결과를 합친다.
  */
 public class Application {
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
+        NumberGenerator numberGenerator = new NumberGenerator();
+        List<Integer> computer = numberGenerator.createNumber();
+
+        Referee referee = new Referee();
+
+        String result = "";
+        while(!result.equals("0볼 3스트라이크")) {
+            result = referee.compare(computer, askNumbers());
+            System.out.println(result);
+        }
+
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
     }
+
+    public static List<Integer> askNumbers() {
+        System.out.println("숫자를 입력해 주세요 : ");
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.next();
+
+        List<Integer> numbers = new ArrayList<>();
+        for(String number : input.split("")) {
+            numbers.add(Integer.valueOf(number));
+        }
+
+        return numbers;
+    }
+
+
 }
