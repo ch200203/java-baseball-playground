d9277b1186d14dae4501008cab08f134000a2b68
ch200203 <ch200205@naver.com>
2023-06-27 02:07:58 +0900

    test(baseball) : 숫자비교 테스트

--- src/test/java/baseball/domain/RefereeTest.java
+++ src/test/java/baseball/domain/RefereeTest.java
@@ -4,17 +4,34 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.Arrays;
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class RefereeTest {
+    private Referee referee;
+    private static final List<Integer> ANSWER = Arrays.asList(1,2,3);
+
+    @BeforeEach
+    void setUp() {
+        referee = new Referee();
+    }
 
     @Test
-    void compare() {
-        Referee referee = new Referee();
-        String result = referee.compare(Arrays.asList(1,2,3), Arrays.asList(1,2,3));
+    void 스트라이크3() {
+        String result = referee.compare(ANSWER, Arrays.asList(1,2,3));
         assertThat(result).isEqualTo("0볼 3스트라이크");
+    }
 
-        String result2 = referee.compare(Arrays.asList(1,2,3), Arrays.asList(4,5,6));
-        assertThat(result2).isEqualTo("낫싱");
+    @Test
+    void 낫싱() {
+        String result = referee.compare(ANSWER, Arrays.asList(4,5,6));
+        assertThat(result).isEqualTo("낫싱");
+    }
+
+    @Test
+    void 볼3() {
+        String result = referee.compare(ANSWER, Arrays.asList(3,1,6));
+        assertThat(result).isEqualTo("2볼 0스트라이크");
     }
 }
